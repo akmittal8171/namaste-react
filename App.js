ffebc98eb1ca80dd6cfecbfcf306815c3a029c04
akmittal8171 <akritimittal1979@gmail.com>
2024-01-11 19:12:13 +0530

    changed html content

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
-const heading=React.createElement("h1",{},"Hello world")
+const heading=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"Hell0 1"),
+        React.createElement("h2",{},"Hello world 1")
+    ]),
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"Hell0 2"),
+        React.createElement("h2",{},"Hello world 2")
+    ])
+])
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 
