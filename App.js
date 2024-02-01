9e50832a15d82480fc1274ccf14577b5860d18c5
akmittal8171 <akritimittal1979@gmail.com>
2024-02-01 16:30:36 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-const heading=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Hell0 1"),
-        React.createElement("h2",{},"Hello world 1")
+import React from "react"
+import ReactDOM from "react-dom/client"
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "Hell0 1"),
+        React.createElement("h2", {}, "Hello world 1")
     ]),
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Hell0 2"),
-        React.createElement("h2",{},"Hello world 2")
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "Hell0 2"),
+        React.createElement("h2", {}, "Hello world 2")
     ])
 ])
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
+// jsx => babel transpiles it to React.createElement => ReactElement- js object => HTMLElement(render)
 
-root.render(heading)
+const jsxHeading = <h1>namaste React using JSX 🚀</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(jsxHeading)
